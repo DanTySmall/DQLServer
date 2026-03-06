f6e6cf0ad87504945dab2c926a51e50e9a96dc62
Daniel T. Small <DanielTySmall@outlook.com>
2026-03-06 15:45:07 -0800

    Parser can detect SELECT Keyword

--- Parser.java
+++ Parser.java
@@ -9,11 +9,31 @@ public class Parser{
 
     public List<Character> parse(String file){
 
-        try(BufferedReader reader = new BufferedReader(new FileReader("file.txt"))){
+        try(BufferedReader reader = new BufferedReader(new FileReader(file))){
+            StringBuilder sb = new StringBuilder();
             int c;
+
+            // Loops and parses the text
             while ((c = reader.read()) != -1){
+
+                // Detect Whitespace
+                if(c == '\n' || c ==' ' || c == '\t'){
+                    String lexeme = sb.toString().toUpperCase();
+                    if (lexeme.equals("SELECT")){
+                        System.out.println("SELECT TOKEN DETECTED");
+                    }
+                    System.out.print('\n');
+                    sb.setLength(0);
+                    continue;
+                }else{
+                    sb.append((char) c);
+                }
                 System.out.print((char) c);
             }
+
+            // Check For the Last Word
+            System.out.print('\n');
+
 
         } catch (IOException e){
             e.printStackTrace();
